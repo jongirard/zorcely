1242e478eebb4594d6b75ef9391410a0653a44ca
Jon Girard <jongirard03@gmail.com>
2016-05-16 03:15:07 -0700

    plans styling and registration functionality

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'high_voltage', '~> 3.0.0'
 gem 'bootstrap', '~> 4.0.0.alpha3'
 gem 'devise'
+gem 'figaro'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,7 +4,26 @@ class RegistrationsController < Devise::RegistrationsController
     if @plan && ENV['PLANS'].include?(@plan) && @plan != "admin"
       super
     else
-      redirect_to page_path('plans')
+      super
     end
   end
+
+  def create
+    super
+  end
+
+  protected
+
+  def check_param
+    @plan = params[:plan]
+    if @plan && ENV['PLANS'].include?(@plan)
+    else
+      redirect_to page_path('plans')
+      flash[:error] = "There was a problem creating your account. Please try again or <a href=\"mailto:support@goplentus.com\">Contact Support</a>."
+  end
+end
+
+  def after_inactive_sign_up_path_for(resource)
+      new_user_session_path
+    end
 end
